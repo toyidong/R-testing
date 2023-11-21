becb4a4ebf28711c17d7ecfa766b733745c0e3e3
Yi Dong <toyidong@gmail.com>
2023-11-20 23:40:19 -0500

    Update test.r

--- test.r
+++ test.r
@@ -9,29 +9,40 @@ number_of_blocks <- subjects/blocksize
 # Set seed for reproducibility (optional)
 set.seed(123)
 
-# Create an empty list to store blocks
+# Create an empty list to store final results
 sites <- list()
 
 for(site in 1:number_of_site){
+
+    # Create site name for each site
     site_name <- ifelse(site == 1, "AAA", "BBB")
+
+    # Create an empty list to store blocks
     blocks <- list()
 
+    # for loop to append all randomized vectors to blocks list
     for(block in 1:number_of_blocks ){
-    element <- c(rep("T",blocksize*T/(T+C)), rep("C", blocksize*C/(T+C)))
+        # Create a vector of element
+        element <- c(rep("T",blocksize*T/(T+C)), rep("C", blocksize*C/(T+C)))
 
-    shuffled_numbers <- sample(element, replace=FALSE)
-    blocks <- append(blocks,list(shuffled_numbers))
+        # randomize the element vector
+        shuffled_numbers <- sample(element, replace=FALSE)
+        # append the randomized vector to blocks list
+        blocks <- append(blocks,list(shuffled_numbers))
     }
 
-    for(i in seq_along(blocks)){
-        for(j in seq_along(blocks[[i]])) {
-        sites <- append(sites, paste0(site_name, (i-1)*blocksize + j, blocks[[i]][j]))
+    # after all randomized vector has been appended into blocks list
+    # concatenate site_name, sequence number of each element, and element to one string
+    for(i in seq_along(blocks)){ # loop through the blocks to get each randomized vector
+        for(j in seq_along(blocks[[i]])) { # loop through the vector to get each element
+            # concatenate by paste0
+            # append concatenated result to sites list
+            sites <- append(sites, paste0(site_name, (i-1)*blocksize + j, blocks[[i]][j]))
         }
     }
 }
 
-
-
+# after all results appended to sites list, print out results
 print(sites)
 
 
